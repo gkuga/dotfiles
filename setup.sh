b275a41668de52cd8034d2f5ddfda2ab65264503
Gaku Yasui <isuya.kuga@gmail.com>
2019-04-13 07:09:52 +0000

    Changed scripts into bash.d directory

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-DOT_FILES=(bashrc screenrc gitconfig git-completion.bash vimrc)
+DOT_FILES=(bashrc screenrc gitconfig vimrc)
 
 for file in ${DOT_FILES[@]}
 do
   ln -fs $HOME/dotfiles/$file $HOME/.$file
 done
+
+SCRIPTS=(git-completion.sh)
+SCRIPTS_HOME=$HOME/.bash.d/
+
+mkdir -p $SCRIPTS_HOME
+for file in ${SCRIPTS[@]}
+do
+  ln -fs $HOME/dotfiles/$file $SCRIPTS_HOME/$file
+done
+
+
