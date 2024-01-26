171d4bb33a30c2fcffec4266e9a9772d306f92a7
Gaku Yasui <gaku.yasui@gmail.com>
2024-01-26 18:48:55 +0900

    Add bash.d

---
+++ bash.d/android.sh
@@ -0,0 +1,6 @@
+export ANDROID_HOME=${HOME}/lib/android-sdk
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
+
+# Flutter
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+
