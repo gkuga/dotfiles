a8c8c7edf955d33d1df60c087386f9b8c9c15ffc
Gaku Yasui <gaku.yasui@gmail.com>
2024-08-13 00:34:57 +0900

    add opam.sh

---
+++ zsh.d/opam.sh
@@ -0,0 +1,7 @@
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+[[ ! -r '/Users/gkuga/.opam/opam-init/init.zsh' ]] || source '/Users/gkuga/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+# END opam configuration
