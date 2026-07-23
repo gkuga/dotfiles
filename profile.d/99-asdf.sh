13645d56f8adc5559198497aea30657e3965ea40
Gaku Yasui <gaku.yasui@gmail.com>
2026-07-23 17:19:36 +0900

    chore: asdfの初期化をshims方式のPATH追加に変更

--- profile.d/99-asdf.sh
+++ profile.d/99-asdf.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-if [ "$(uname)" = "Darwin" ] && [ "$(uname -m)" = "arm64" ]; then
-  . /opt/homebrew/opt/asdf/libexec/asdf.sh
-else
-  . "$HOME/.asdf/asdf.sh"
-  . "$HOME/.asdf/completions/asdf.bash"
-fi
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
